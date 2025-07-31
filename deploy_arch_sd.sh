73c7a65878555bd9231b1a06ac7459fee75b616b
Christian Porter <cporter1993@gmail.com>
2025-07-31 16:22:21 -0400

    update locale lang

--- deploy_arch_sd.sh
+++ deploy_arch_sd.sh
@@ -163,8 +163,9 @@ locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export LANG=en_US.UTF-8
-echo "Server = https://nj.us.mirror.archlinuxarm.org/\$arch/\$repo" >> /etc/pacman.d/mirrorlist
+export LC_ALL=en_US.UTF-8
 
+echo "Server = https://nj.us.mirror.archlinuxarm.org/\$arch/\$repo" >> /etc/pacman.d/mirrorlist
 sed -i '/^
 
 \[options\]
