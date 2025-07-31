bf975e03dcaa651f31a3e543460ab06a14aa1d30
Christian Porter <cporter1993@gmail.com>
2025-07-31 15:35:28 -0400

    update mirrorlist sid for pacman option to disable sandbox

--- deploy_arch_sd.sh
+++ deploy_arch_sd.sh
@@ -163,6 +163,14 @@ locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export LANG=en_US.UTF-8
+echo "Server = https://nj.us.mirror.archlinuxarm.org/\$arch/\$repo" >> /etc/pacman.d/mirrorlist
+
+sed -i '/^
+
+\[options\]
+
+/a DisableSandbox\nDisableDownloadTimeout' /etc/pacman.conf
+echo "Added 'DisableSandbox' and 'DisableDownloadTimeout' to [options] in pacman.conf"
 
 echo "$HOSTNAME" > /etc/hostname
 
